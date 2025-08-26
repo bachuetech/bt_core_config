551cbac6121cd72b9c45b309d88f3a6a076cc454
bachuetech <bachuetech@yahoo.com>
2025-08-26 11:09:34 -0400

    Change visibility of get_app_info but add guards
    get_app_info is pub again
    Added deprecated warning.
    Added cfg(debug_assertions)
    Added a Guard inside the function to print out a message.

--- src/app_info.rs
+++ src/app_info.rs
@@ -18,7 +18,17 @@ impl AppInfo {
     //This seems to be a useless function. But useful as a snippet
     ///Retrieves the BT_CORE_CONFIG crate info.
     /// Useful for testing purpose (e.g. unit test). Do not use in PROD!
-    pub (crate) fn get_app_info(default_name: &'static str, default_version: &'static str, default_authors: &'static str, default_desciption: &'static str) -> Self{
+    #[cfg(debug_assertions)]
+    #[deprecated(
+        since = "0.2.1",
+        note = "This function is for testing only. Avoid using in production.")]
+    pub fn get_app_info(default_name: &'static str, default_version: &'static str, default_authors: &'static str, default_desciption: &'static str) -> Self{
+        #[cfg(not(test))]
+        panic!("AppInfo::get_app_info() function is for unit tests only!");
+
+        #[cfg(test)]
+        println!("AppInfo::get_app_info() is safe to run in tests only!");
+
         let pkg_name: &'static str = match option_env!("CARGO_PKG_NAME"){
                                             Some(v) => if v.is_empty() {
                                                                     default_name

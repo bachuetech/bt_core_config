490aa875567f62ff621a6d840322f05b269b2a6d
bachuetech <bachuetech@yahoo.com>
2025-08-27 09:31:32 -0400

    Add get_app_name
    get_app_name: Uses the provided name, but falls back to the executable name

--- src/app_info.rs
+++ src/app_info.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 ///Initialize basic information gathering data from Cargo.toml or default values.
 
 pub struct AppInfo{
@@ -96,4 +98,53 @@ impl AppInfo {
                                         };
         Self { package_name: pkg_name, version: pkg_version, authors: pkg_authors, description: pkg_desc }
     }
+
+    fn get_app_name(package_name: Option<&str>) -> String{
+       let pkg_name = package_name.unwrap_or("").trim();
+       if pkg_name.len() > 0 
+        { pkg_name.to_string() 
+        } else { //Get the name from current executable
+            //gets the full path to the currently running executable.
+            //extracts the filename without its extension (path.file_stem())
+            //remove the suffix
+            let full_name = env::current_exe()
+                .ok()
+                .and_then(|path| path.file_stem().map(|s| s.to_string_lossy().into_owned()))
+                .unwrap_or_else(|| "BT_UnkownApp".to_string())
+                .split('-').next().map(|base| base.to_string()).unwrap_or("BT_UnkownApp".to_owned());
+
+            if full_name.trim().len() > 0{
+                full_name.trim().to_owned()
+            }else{
+                "BT_UnkownApp".to_string()
+            }
+        }
+    }
+}
+
+
+//***********/
+// UNIT TEST 
+//***********/
+#[cfg(test)]
+mod app_config_tests {
+    use crate::app_info::AppInfo;
+
+    #[test]
+    pub fn test_get_app_name_with_package_success(){
+        let name = AppInfo::get_app_name(Some("test"));
+        assert_eq!(name,"test");
+    }
+
+    #[test]
+    pub fn test_get_app_name_with_none_success(){
+        let name = AppInfo::get_app_name(None);
+        assert_eq!(name,"bt_core_config");
+    }
+
+    #[test]
+    pub fn test_get_app_name_with_empty_success(){
+        let name = AppInfo::get_app_name(Some(""));
+        assert_eq!(name,"bt_core_config");
+    }       
 }
